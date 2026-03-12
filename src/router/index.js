f4048a6ff651d2714a3a3d97d321867bb3dc578c
Francis <36138122+fpamadeo@users.noreply.github.com>
2026-03-12 17:08:35 -0500

    initial files

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import ExperiencePage from '@/views/ExperiencePage.vue'
+import AboutPage from '@/views/AboutPage.vue'
+import OtherPage from '@/views/OtherPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Experience',
+    component: ExperiencePage,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutPage,
+  },
+  {
+    path: '/other',
+    name: 'Other',
+    component: OtherPage,
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 }
+  },
 })
 
 export default router
